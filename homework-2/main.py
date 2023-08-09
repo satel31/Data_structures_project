da176ba22a3db3f23ba172bd4201b7d471b0dec8
Tatiana Blokhnina <blokhnina.tatiana@yandex.ru>
2023-08-10 00:01:02 +0300

    Update main.py

--- homework-2/main.py
+++ homework-2/main.py
@@ -5,10 +5,10 @@ if __name__ == '__main__':
     stack.push('data1')
     data = stack.pop()
 
-    # стэк стал пустой
+    # Stack became empty
     assert stack.top is None
 
-    # pop() удаляет элемент и возвращает данные удаленного элемента
+    # pop() deletes elements and return data
     assert data == 'data1'
 
     stack = Stack()
@@ -16,8 +16,6 @@ if __name__ == '__main__':
     stack.push('data2')
     data = stack.pop()
 
-    # теперь последний элемента содержит данные data1
     assert stack.top.data == 'data1'
 
-    # данные удаленного элемента
     assert data == 'data2'
