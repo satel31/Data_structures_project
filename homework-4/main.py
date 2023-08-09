a1de11c65792cbe0a0f3d2d63f8459a61b81fbdb
Tatiana Blokhnina <blokhnina.tatiana@yandex.ru>
2023-08-10 00:01:58 +0300

    Update main.py

--- homework-4/main.py
+++ homework-4/main.py
@@ -1,10 +1,8 @@
 from src.queue import Queue
 
 if __name__ == '__main__':
-    # Создаем пустую очередь
     queue = Queue()
 
-    # Добавляем данных в очередь
     queue.enqueue('data1')
     queue.enqueue('data2')
     queue.enqueue('data3')
